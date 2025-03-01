8aba83f1715c8a8b37e92573b1b172b4037503f8
Oyewale Dominion <limbotech116@gmail.com>
2025-03-01 02:25:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <main className="">
+      <div>
+        <section className="mt-12">Hero</section>
+        <section className="p-4">PopularProducts</section>
+        <section className="p-4">SuperQuality</section>
+        <section className="p-4">Services</section>
+        <section className="p-4">SpecialOffers</section>
+      </div>
+    </main>
+  );
+};
+
+export default App;
